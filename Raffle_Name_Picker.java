a85e2e3a818142435bece5cf062d9c5d83b3dbd5
Kooreis <kooreis.official@gmail.com>
2023-01-14 01:23:11 -0600

    Defines the main class and main method, and initializes the necessary variables.

--- Raffle_Name_Picker.java
+++ Raffle_Name_Picker.java
@@ -1,3 +1,5 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
+public class RaffleNamePicker {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
